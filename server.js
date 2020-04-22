403b09f95a5b438b5dce43a63a7936bf72781686
xiaolu119 <60416292+xiaolu119@users.noreply.github.com>
2020-04-22 00:18:52 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const htmlroute = require("./route/htmlroute");
+const apiroute = require("./route/apiroute");
+const PORT = 8080;
+const app = express("");
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/", htmlroute);
+app.use("/api", apiroute);
+
+
+
+
+
+app.listen(PORT,() => {
+    console.log ("serverlisening", PORT)
+});
